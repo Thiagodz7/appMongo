a0b0ec9c302a06173e40a246861391209629921f
Thiago Rodrigues de S Silva <thiago.sousa@castgroup.com.br>
2022-12-14 11:25:03 -0300

    iyfi

--- app/app.js
+++ app/app.js
@@ -16,60 +16,22 @@ app.get("/",async (req, res) => {
             logStrem.write('\n');
 
             const connection = () => MongoClient
-                      .connect("mongodb://adm:123@127.0.0.1:27017/usuario", 
+                      .connect("mongodb://adm:123@localhost:27017", 
                                 {
                                   useNewUrlParser: true,
                                   useUnifiedTopology: true
                                 })
-                                .then((conn) => conn.db("usuario"))
+                                .then((conn) => conn.db("usuario"), console.log("conectado!"))
                                 .catch((err) => {
                                   console.error(err);
                                   process.exit(1);
                                 });
 
-           
-
             const db = await connection();
-             var users = db.collection("usuario").find().toArray();
+             var users = await db.collection("usuario").find().toArray();
 
-            res.status(200).json(users);
+            //res.status(200).json(users);
             res.send(users);
-            // mongoose.connect(
-            //   process.env.MONGODB_URI || "mongodb://adm:123@localhost:27017/usuario",
-            //   {
-            //     useNewUrlParser: true,
-            //     useUnifiedTopology: true,
-            //   },
-            //   async (err) => {
-            //     if (err) {
-            //       console.log(err);
-            //     } else {
-            //       const usuarioSchema = new mongoose.Schema({
-            //         nome: String,
-            //         idade: Number
-            //       }, { collection: 'usuario' }
-            //       );
-      
-            //       const usuarios = mongoose.model('usuario', usuarioSchema, 'usuario');
-       
-            //       const docs = await usuarios.find({}).lean().exec();
-            //       res.send("Conectado com sucesso\n", docs);
-            //     }
-            //   }
-            // );
-
-            // var mongoose = require('mongoose')
-            // mongoose.connect("mongodb://adm:123@localhost:27017")
-            // mongoose.connection.on('error', () => console.error('connection error:'))
-            // mongoose.connection.on('open', () => console.error('connected'))
-
-            // try{
-            //   const usuario = await "usuario".find()
-            //   res.send(usuario)
-            // }catch(error){
-            //   res.send(500,error)
-            // }
-
 });
 
 app.listen(porta, () => {
